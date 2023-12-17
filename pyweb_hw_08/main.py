3f2194819476248fe04e83ea1423a6a4d9ab417a
Spica12 <vitaly.savenko12@gmail.com>
2023-12-17 11:07:44 +0200

    finish lesson m16_02

--- pyweb_hw_08/main.py
+++ pyweb_hw_08/main.py
@@ -1,1 +1,39 @@
-from mongoengine import Document, connect
+import redis
+from redis_lru import RedisLRU
+from models import Author, Quote
+from pprint import pprint
+
+client = redis.StrictRedis(host="localhost", port=6379, password=None)
+cache = RedisLRU(client)
+
+
+@cache
+def find_by_tag(tag: str) -> list[str | None]:
+    print(f"Finding by {tag}")
+    quotes = Quote.objects(tags__iregex=tag)
+    result = [quote.quote for quote in quotes]
+
+    return result
+
+
+@cache
+def find_by_authors(author: str) -> list[str | None]:
+    print(f"Finding by {author}")
+    authors = Author.objects(fullname__iregex=author)
+    result = {}
+    for author in authors:
+        quotes = Quote.objects(author=author)
+        result[author.fullname] = [quote.quote for quote in quotes]
+
+    return result
+
+
+if __name__ == "__main__":
+    pprint(find_by_tag("li"))
+    pprint(find_by_tag("li"))
+
+    pprint(find_by_authors("er"))
+    pprint(find_by_authors("er"))
+
+    quotes = Quote.objects().all()
+    pprint([quote.to_json() for quote in quotes])
--- pyweb_hw_08/models.py
+++ pyweb_hw_08/models.py
@@ -8,6 +8,7 @@ from mongoengine import (
 )
 import configparser
 import pathlib
+from bson import json_util
 
 file_config = (
     pathlib.Path(__file__).parent.parent.joinpath("configs").joinpath("config.ini")
@@ -23,7 +24,8 @@ uri = f"mongodb+srv://{user}:{password}@cluster0.ae7t2cd.mongodb.net/?retryWrite
 print(uri)
 
 connect(db=db, host=uri)
-print('Connected')
+print("Connected")
+
 
 class Author(Document):
     fullname = StringField(required=True, unique=True)
@@ -40,3 +42,8 @@ class Quote(Document):
     quote = StringField()
 
     meta = {"collection": "qoutes"}
+
+    def to_json(self, *args, **kwargs):
+        data = self.to_mongo(*args, **kwargs)
+        data["author"] = self.author.fullname
+        return json_util.dumps(data, ensure_ascii=False)
